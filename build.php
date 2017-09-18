0e2db688064b7ab1a4333c6a8a87f33a59b7f1d3
pengs <2563381907@qq.com>
2017-09-18 16:14:00 +0800

    测试一下子

--- build.php
+++ build.php
@@ -12,7 +12,7 @@
 return [
     // 生成应用公共文件
     '__file__' => ['common.php', 'config.php', 'database.php'],
-
+    'peng'=>['admin'],
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
     'demo'     => [
         '__file__'   => ['common.php'],
